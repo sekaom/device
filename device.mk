1e57f668daedb8dbce46d35e944770c1c2870794
sekaom <dulin081682@126.com>
2023-05-15 16:28:59 +0000

    X504F : add sensors copy out rules

--- device.mk
+++ device.mk
@@ -9,3 +9,16 @@ LOCAL_PATH := device/lenovo/X504F
 
 # Device is a Tablet
 PRODUCT_CHARACTERISTICS := tablet
+
+# Sensors
+PRODUCT_PACKAGES += \
+    android.hardware.sensors@1.0-impl \
+    android.hardware.sensors@1.0-service
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf \
+    $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf 
+
+
+    
+    
